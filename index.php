ad4b32442b1d4edde2a24d3d529b647948cba587
thiri sandar <thiris613@gmail.com>
2024-07-18 18:33:53 +0630

    added files

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coffee</title>
+
+  <!-- font awesome cdn link -->
+  <script src="https://kit.fontawesome.com/71aa3b06af.js" crossorigin="anonymous"></script>
+
+  <!-- css file link -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+<!-- header section starts -->
+<header class="header">
+  
+  <a href="#" class="logo">
+    <img src="images/coffee-logo.png" alt="logo" srcset="">
+  </a>
+
+  <nav class="navbar">
+    <a href="#home">Home</a>
+    <a href="#items">Items</a>
+    <a href="#about">About</a>
+    <a href="#review">Review</a>
+    <a href="#contact">Contact</a>
+  </nav>
+
+  <div class="icons">
+    <i class="fa-solid fa-bars menu-btn"></i>
+  </div>
+</header>
+<!-- header section ends -->
+
+<!-- home section starts -->
+
+<section class="home" id="home">
+  <div class="content">
+    <h3>Coffee in the morning</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque magnam corrupti illum, voluptate numquam cupiditate sint at veritatis adipisci quasi. Ut labore repellendus beatae explicabo aliquam, voluptates cupiditate debitis nostrum.</p>
+    <a href="">get yours now</a>
+  </div>
+</section>
+
+<!-- home section ends -->
+<!-- slogan section starts -->
+<section class="slogan">
+  <h3 class="title">Coffee Tips</h3>
+  <p class="caption">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+  <div class="slogan-container">
+
+
+    <div class="slogan-box">
+      <h3 class="slogan-title">For People with love coffee</h3>
+      <p class="slogan-content">Costa Coffee: “Love for the bean” - This tagline emphasizes Costa's passion for quality coffee beans and the art of coffee making. Peet's Coffee: “The original The best coffee in the city. Survive the weekdays; Wake up to life.</p>
+    </div>
+
+    <div class="slogan-box">
+      <h3 class="slogan-title">Coffe with examples</h3>
+      <p class="slogan-content">Home of the world's finest coffees. Live now, Sleep Tomorrow; Your cup of inspiration. Everyone Deserves the COffee Time; Remolding Your Dreams; Only Coffee .</p>
+    </div>
+
+    <div class="slogan-box">
+      <h3 class="slogan-title">Every coffee helps</h3>
+      <p class="slogan-content">Coffee slogans can be an effective way to capture the attention of potential customers and create a memorable brand image for your coffee business. A catchy ...</p>
+    </div>
+
+  </div>
+</section>
+<!-- menu section starts -->
+<section class="menu" id="items">
+
+  <h1 class="heading">Explore the recent products<span></span></h1>
+  <p class="caption">For more products , click add-to-cart to search</p>  
+  <div class="box-container">
+    <!-- <div class="box">
+      <img src="images/menu-1.png" alt="">
+      <h3>tasty and healthy</h3>
+      <div class="price">$10.8 <span>23.99</span></div>
+    </div>
+    <div class="box">
+      <img src="images/menu-2.png" alt="">
+      <h3>tasty and healthy</h3>
+      <div class="price">$10.8 <span>23.99</span></div>
+    </div> 
+    <div class="box">
+      <img src="images/menu-3.png" alt="">
+      <h3>tasty and healthy</h3>
+      <div class="price">$10.8 <span>23.99</span></div>
+    </div> 
+    <div class="box">
+      <img src="images/menu-4.png" alt="">
+      <h3>tasty and healthy</h3>
+      <div class="price">$10.8 <span>23.99</span></div>
+    </div> 
+    <div class="box">
+      <img src="images/menu-5.png" alt="">
+      <h3>tasty and healthy</h3>
+      <div class="price">$10.8 <span>23.99</span></div>
+    </div> 
+    <div class="box">
+      <img src="images/menu-6.png" alt="">
+      <h3>tasty and healthy</h3>
+      <div class="price">$10.8 <span>23.99</span></div>
+    </div> -->
+  </div>
+</section>
+
+<!-- menu section ends -->
+
+<!-- about section starts -->
+
+<section class="about" id="about">
+  <h1 class="heading"> <span>about</span> us </h1>
+
+  <div class="row">
+      <div class="image">
+        <img src="images/about.jpg" alt="" class="about-img">
+      </div>
+      <div class="content">
+        <h3>what makes our coffee special?</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius id dolores illo ea porro laborum facilis, laudantium inventore! Tempora sequi, laudantium repellendus saepe corrupti soluta quis quidem est ratione ipsum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, quaerat voluptatum? Sapiente inventore placeat quos nam quaerat? Quia, molestiae ad.</p>
+        <a href="signup.php" class="btn">Become a Membership</a>
+      </div>
+  </div>
+</section>
+<!-- about section ends -->
+
+<!-- feedback section starts -->
+
+<section class="review" id="review">
+
+  <h1 class="heading">feedback</h1>
+
+  <div class="box-container">
+
+    <div class="box">
+      <img src="images/quote.png" alt="">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus nam minus animi veritatis, consectetur voluptatibus tenetur alias distinctio, maxime eius facere inventore dignissimos saepe voluptas labore quam expedita nisi dolorum?</p>
+      <h3>-- john doe --</h3>
+    </div>
+    <div class="box">
+      <img src="images/quote.png" alt="">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus nam minus animi veritatis, consectetur voluptatibus tenetur alias distinctio, maxime eius facere inventore dignissimos saepe voluptas labore quam expedita nisi dolorum?</p>
+      <h3>-- john doe --</h3>
+    </div> 
+    <div class="box">
+      <img src="images/quote.png" alt="">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus nam minus animi veritatis, consectetur voluptatibus tenetur alias distinctio, maxime eius facere inventore dignissimos saepe voluptas labore quam expedita nisi dolorum?</p>
+      <h3>-- john doe --</h3>
+    </div>
+  </div>
+
+</section>
+
+<!-- feedback section ends -->
+
+<!-- contact section starts -->
+
+<section class="contact" id="contact">
+  <h1 class="heading"><span>contact</span> us</h1>
+  <div class="row">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96729.04650511633!2d-74.07760278740872!3d40.74480671076137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259f809dae747%3A0xcc77ed69118b67af!2sCopper%20Mug%20Coffee!5e0!3m2!1sen!2smm!4v1720954138773!5m2!1sen!2smm" width="550" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="info">
+      <p>Opening Hours: 7:00 AM - 11:00 AM</p>
+      <p>Monday - Sunday</p>
+      <br>
+      <p>Address - Somewhere U.S</p>
+      <p>Phone: +95 9237555698</p>
+      <p class="email">Email: example@gmail.com</p>
+      
+      <i class="fa-brands fa-facebook-f"></i>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-x-twitter"></i>
+      <i class="fa-brands fa-snapchat"></i>
+    </div>
+  </div>
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts -->
+
+<section class="footer">
+  <div class="credit">Copyright &copy; | All Rights Reserved</div>
+</section>
+
+
+
+<!-- footer section ends -->
+
+  <!-- javascript file link -->
+  <script src="js/app.js"></script>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,96 @@
+
+  // Start auto background
+  
+  function* genfun(){
+    var index = 1;
+
+    while(true){
+        yield index++;
+
+        if(index > 6){
+            index = 1;
+        }
+    }
+}
+
+var getgen = genfun();
+// console.log(getgen.next().value);
+
+
+var getheader = document.querySelector('.home');
+getheader.style.backgroundImage = `url('./assets/img/banner/bannercoffee${getgen.next().value}.jpg')`;
+
+
+function autoload(){
+    getheader.style.backgroundImage = `url('./assets/img/banner/bannercoffee${getgen.next().value}.jpg')`;
+} 
+
+
+setInterval(autoload,3000);
+
+
+// End auto background
+
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('.icons').onclick = () => {
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  navbar.classList.remove('active');
+}
+
+
+// const getcoffeedatas = fetch("https://fake-coffee-api.vercel.app/api")
+//   .then((res) => res.json())
+//   .then((data) => data);
+
+var url = "https://fake-coffee-api.vercel.app/api";
+
+
+fetch(url)
+        .then(response=>response.json())
+        .then(datas=>{
+                coffeedatatodom(datas);
+            }).
+            catch(err=>console.log(err));
+    
+function redirectToCart(){
+  window.location.href = "addtocart.php";
+}
+
+function coffeedatatodom(coffees) {
+
+  let count = 0;
+
+coffees.forEach(function(coffee) {
+    if (count >= 6) return; // Exit the function if count reaches 12
+    
+    var getcoffeestring = JSON.stringify(coffee);
+    var coffeeObject = JSON.parse(getcoffeestring);
+    
+    var lis = document.createElement('li');
+    lis.classList.add('box');
+    console.log(coffeeObject);
+    // Access the properties directly from the parsed object
+    lis.innerHTML = `
+      <img src="${coffeeObject.image_url}" width="400px" height="200px" />
+      <h3>${coffeeObject.name}</h3>
+      <p>Price <span class="price"> ${Math.round(coffeeObject.price * 200)} mmk</span></p>
+      <p>Made by <span class="madeby">${coffeeObject.region}</span></p>
+      <button type="button" class="btncarts" onclick="redirectToCart()">Add to cart <i class="fas fa-trash-alt icons"></i></button>
+    `;
+    
+    document.querySelector('.box-container').appendChild(lis);
+    
+    count++; // Increment the counter
+
+    
+});
+
+
+    
+}
